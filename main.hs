979d9919bd50eb73be4af42559eb3b3dcb8128e4
Jacob Meixner <jjm3333@gmail.com>
2017-10-10 20:36:13 -0700

    Create a new function to pull out the kth element in a list

--- main.hs
+++ main.hs
@@ -7,8 +7,13 @@ data Card = Card Value Suit deriving (Show)
 
 main :: IO()
 main = do
-	putStrLn "Hello Sheepshead"
-	putStrLn $ "Here is my first card: " ++ (show $ Card Queen Spades )
+  putStrLn "Hello Sheepshead"
+  putStrLn $ "Here is my first card: " ++ (show $ Card Queen Spades )
+  
+removeAt 0 (x:xs) = (x,xs)
+removeAt n [] = (0, []) -- should do something more for out of bounds
+removeAt n (x:xs) = let (out, rest) = removeAt (n-1) xs
+                    in (out, x:rest)
 
 allSuits = [Clubs ..]
 allValues = [Seven ..]
